e9c49ec00222d3e2a28e37654c9cc2fe2edee144
Risatommy <risa555@gmail.com>
2014-04-24 12:35:40 +0900

    happyを加えました

--- js/helloworld.js
+++ js/helloworld.js
@@ -5,3 +5,11 @@ var helloWorld = function(){
 
 var showHelloWorldButton = document.getElementById("showHelloWorld");
 showHelloWorldButton.addEventListener("click", helloWorld);
+
+var hellohappy = function(){
+	var message = document.getElementById("hellohappy");
+	message.setAttribute("class", "");
+};
+
+var showHellohappyButton = document.getElementById("showHellohappy");
+showHellohappyButton.addEventListener("click", hellohappy);
